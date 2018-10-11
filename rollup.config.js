92cc571d7ef3ac6594783af188abb59f36375af9
Ryan Walters <shout@ryanwalters.co>
2018-10-10 22:25:32 -0500

    set up repo to try to reproduce rollup/rollup-plugin-babel#236

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,24 @@
 import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
-import pkg from './package.json';
 
 export default [
-	// browser-friendly UMD build
 	{
 		input: 'src/main.js',
+		experimentalCodeSplitting: true,
+		experimentalPreserveModules: true,
 		output: {
-			name: 'howLongUntilLunch',
-			file: pkg.browser,
-			format: 'umd'
+			dir: 'dist',
+			format: 'es'
 		},
 		plugins: [
+			babel({
+        plugins: [
+          '@babel/plugin-proposal-object-rest-spread',
+        ],
+      }),
+      commonjs(), // so Rollup can convert `ms` to an ES module
 			resolve(), // so Rollup can find `ms`
-			commonjs() // so Rollup can convert `ms` to an ES module
 		]
 	},
-
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify 
-	// `file` and `format` for each target)
-	{
-		input: 'src/main.js',
-		external: ['ms'],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-		]
-	}
 ];
